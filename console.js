60472bc7e51929279fbc56732f2e74490617f870
ichirohn <32905698+ichirohn@users.noreply.github.com>
2022-10-26 17:06:42 +0700

    Update console.js

--- console.js
+++ console.js
@@ -1,6 +1,6 @@
 -// hey this is a comment
 -console.log("Hello World")
--console.log("Hello World") // 2nd time!!!!
+-console.log("Hello World") // 2nd time!!!!!
 +console.log("Hello World");
 
 function main(){
