39083edc857bcc8ce6ebb1670fb6be18663276bc
KarboneDEV <68824394+Karbone-DEV@users.noreply.github.com>
2022-12-25 18:46:59 +0100

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,8 @@
 -// hey this is a comment
 -console.log("Hello World")
 -console.log("Hello World") // 2nd time!!!!
++console.log("Hello World");
+
+function main(){
+-return null;
++ console.log (1);
